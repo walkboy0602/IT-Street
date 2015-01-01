75812d502619fe7615e1146a45bf275e9d5c1a51
Khoo <walkboy0602@gmail.com>
2015-01-01 23:22:17 +0800

    Initial Project

---
+++ IT-Street/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="IT_Street.MvcApplication" Language="C#" %>
---
+++ IT-Street/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(IT_Street.Startup))]
+namespace IT_Street
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
